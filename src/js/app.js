cac0f4d05b3d8916f552a4538732274dc88ec336
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-22 15:15:47 -0600

    get vuetify rolling

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
-import App from '../Components/App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+import App from '../Components/App'
+
+Vue.use(Vuetify)
 
 new Vue({
+  vuetify: new Vuetify(),
   render: createElement => createElement(App)
 }).$mount('#app')
